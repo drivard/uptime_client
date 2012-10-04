a3216a529f17d6b714cf5e07bc1ef5fb5790998f
Dominick Rivard <dominick.rivard@gmail.com>
2012-10-04 00:10:51 -0400

    Add:: configuration file settings.

--- uptime/__init__.py
+++ uptime/__init__.py
@@ -8,4 +8,9 @@ Author: Dominick Rivard <dominick.rivard@gmail.com>
 """
 
 __version__ = '0.1'
+
 __website_name__ = 'openuptimz.com'
+
+__config_file__ = '/.uptime/uptime.conf'
+
+__config_folder__ = '/.uptime/'
