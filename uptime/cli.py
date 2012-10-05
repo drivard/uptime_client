1fa8c14680cc186a613d697c11fa86dea2ba62de
Dominick Rivard <dominick.rivard@gmail.com>
2012-10-04 23:03:20 -0400

    Pep8:: pep8 it.

--- uptime/cli.py
+++ uptime/cli.py
@@ -12,117 +12,117 @@ DESCRIPTION += " computer to the {website} website."
 DESCRIPTION = DESCRIPTION.format(website=WEBSITE_NAME)
 USAGE = '''\t%prog [options]'''
 
+
 def display_options(options):
     '''
-    
-    This function displays all the attributes that the uptime client 
+
+    This function displays all the attributes that the uptime client
     gather and pushes to the website, based on the desired options.
-    
+
     e.g.:
         options::['host','arch','dist','uptime','version',]
-    
+
     '''
-    
+
     to_print = '\n\t'
-    
+
     if "host" in options:
         to_print += colorize("Computer:\t\t", CYAN)
         to_print += hostname_handler() + '\n\t'
-        
+
     if "uptime" in options:
         days, hours, min, sec, c_uptime, raw_seconds = uptime_handler()
         to_print += colorize("Uptime:\t\t\t", CYAN)
         to_print += c_uptime + '\n\t'
-        
+
     if "dist" in options:
         to_print += colorize("Distribution:\t\t", CYAN)
         to_print += distribution_handler() + '\n\t'
-    
-    if "version" in options:  
+
+    if "version" in options:
         to_print += colorize("Distribution version:\t", CYAN)
         to_print += version_handler() + '\n\t'
-        
+
     if "arch" in options:
         to_print += colorize("Architecture:\t\t", CYAN)
         to_print += architecture_handler() + '\n\t'
-    
+
     return to_print
+
 
 def main():
     '''
-    
+
     Command-line tool to configure and test the daemon
     to update the uptime of your computer to the website.
-    
+
     '''
-    
+
     parser = optparse.OptionParser(
         usage=infos(USAGE),
         description=colorize(DESCRIPTION, PURPLE),
-        add_help_option=False
-    )
-    
+        add_help_option=False)
+
     parser.add_option(
-        "-h", "--help", 
+        "-h", "--help",
         action="store_true", dest="help",
-        help="Show this help message and exit."
-    )
-    
+        help="Show this help message and exit.")
+
     parser.add_option(
-        "-a", "--all", 
+        "-a", "--all",
         action="store_true", dest="all",
         help='''Return all the information available for this client.
         Does not include the option "-p".'''
     )
-    
+
     parser.add_option(
-        "-r", "--arch", 
+        "-r", "--arch",
         action="store_true", dest="architecture",
         help="Return the architecture of the hardware platform."
     )
-    
+
     parser.add_option(
-        "-d", "--distribution", 
+        "-d", "--distribution",
         action="store_true", dest="distribution",
         help="Return the distribution name."
     )
-    
+
     parser.add_option(
-        "-l", "--dist-version", 
+        "-l", "--dist-version",
         action="store_true", dest="dist_version",
         help="Return the distribution version number."
     )
-    
+
     parser.add_option(
-        "-n", "--hostname", 
-        action="store_true", dest="hostname", 
+        "-n", "--hostname",
+        action="store_true", dest="hostname",
         help="Return the hostname of the computer."
     )
-    
+
     parser.add_option(
-        "-p", "--push", 
+        "-p", "--push",
         action="store_true", dest="push",
         help="Push the uptime information to the website."
     )
-    
+
     parser.add_option(
-        "-u", "--uptime", 
-        action="store_true", dest="uptime", 
+        "-u", "--uptime",
+        action="store_true", dest="uptime",
         help="Returns the uptime of the computer."
     )
-    
+
     parser.add_option(
-        "-v", "--version", 
+        "-v", "--version",
         action="store_true", dest="version",
         help="Returns the version of the client."
     )
-    
+
     '''
     If an option is selected, the options dictionary will be set with
     a key/value pair of the key being the name of the option and the
     the value being True or None.
-    
-    e.g.: 
+
+    e.g.:
         options = {
                     'all': None,
                     'dist_version': None,
@@ -134,55 +134,54 @@ def main():
                     'push': None,
                 }
     '''
-    
+
     options, args = parser.parse_args()
     options_to_display = ['', ]
     number_of_options = 0
-    
+
     if options.all:
-        options_to_display = ['host','arch','dist','uptime','version',]
+        options_to_display = ['host', 'arch', 'dist', 'uptime', 'version', ]
         number_of_options += 1
-        
+
     if options.architecture:
         options_to_display.append('arch')
         number_of_options += 1
-        
+
     if options.distribution:
         options_to_display.append('dist')
         number_of_options += 1
-        
+
     if options.dist_version:
         options_to_display.append('version')
         number_of_options += 1
-        
+
     if options.hostname:
         options_to_display.append('host')
         number_of_options += 1
-        
+
     if options.uptime:
         options_to_display.append('uptime')
         number_of_options += 1
-        
+
     if options.version:
         print VERSION
         number_of_options += 1
-    
+
     # Display the requested options
     if options_to_display != ['', ]:
         print display_options(options_to_display)
         number_of_options += 1
-    
+
     if options.push:
         print 'Will push infos to website'
         number_of_options += 1
-    
+
     if options.help or number_of_options == 0:
         parser.print_help()
         print '\n'
-    
-        
+
     return sys.exit(2)
-    
-   
+
+
 if __name__ == "__main__":
     main()
--- uptime/colours.py
+++ uptime/colours.py
@@ -7,47 +7,50 @@ Let's put some colour to our digital life!
 '''
 
 ENDC = '\033[0m'
-BLACK="\033[30m"        # Black
-RED="\033[31m"          # Red
-GREEN="\033[32m"        # Green
-YELLOW="\033[33m"       # Yellow
-BLUE="\033[34m"         # Blue
-PURPLE="\033[35m"       # Purple
-CYAN="\033[36m"         # Cyan
-WHITE="\033[37m"        # White
+BLACK = "\033[30m"        # Black
+RED = "\033[31m"          # Red
+GREEN = "\033[32m"        # Green
+YELLOW = "\033[33m"       # Yellow
+BLUE = "\033[34m"         # Blue
+PURPLE = "\033[35m"       # Purple
+CYAN = "\033[36m"         # Cyan
+WHITE = "\033[37m"        # White
+
 
 def colorize(message='', colour=ENDC):
     '''
-    
+
     Apply the specific colour to the string message.
-    
+
     '''
     return colour + message + ENDC
-    
+
+
 def warnings(message):
     '''
-    
+
     Apply and display the warning message using the yellow colour.
-    
+
     '''
-    
+
     return colorize(message, YELLOW)
-    
+
+
 def errors(message):
     '''
-    
+
     Apply and display the error message using the red colour.
-    
+
     '''
-    
+
     return colorize(message, RED)
-    
+
+
 def infos(message):
     '''
-    
+
     Apply and display the information message using the green colour.
-    
+
     '''
-    
+
     return colorize(message, GREEN)
-    
