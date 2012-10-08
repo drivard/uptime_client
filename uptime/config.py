4ebd72b4bda70e7418757c1dce77ea74a313f4e9
Dominick <dominick.rivard@gmail.com>
2012-10-08 01:38:12 -0400

    Fix:: remove the filename parameter and concentrated  all the configuration in the default config file.

--- uptime/config.py
+++ uptime/config.py
@@ -3,7 +3,7 @@
 '''
 
 The config library reads the ~/.uptime/uptime.conf file to read
-the user account information that will be used to publish the
+the user account information that will be used to publish the 
 information to the website.
 
 '''
@@ -18,83 +18,85 @@ import os
 
 def check_config_file():
     '''
-
+    
     This function will verify the existance of the configuration file.
     If the file doesn't exist it will create it.
-
-    '''
-
+    
+    '''    
+    
     # Create the folder if it doesn't exists
     if not os.path.isdir(HOME + CONFIG_FOLDER):
         os.mkdir(HOME + CONFIG_FOLDER)
-
+    
     # Create the configuration file if it doesn't exists
     config_file = HOME + CONFIG_FILE
-
+    
     if not os.path.isfile(config_file):
         with open(config_file, 'w') as file:
             file.close()
-
+    
     return config_file
 
 
-def load_config_file(filename):
+def load_config_file():
     '''
-
+    
     This function load all the configuration from the file located in
     ~/.uptime/uptime.conf and return all the information.
-
+    
     '''
-
+    
     config = ConfigParser()
-    config.read(filename)
-
+    config.read(HOME + CONFIG_FILE)
+    
     sections = config.sections()
-
+    
     options = []
-
+    
     for s in sections:
         options.extend(config.items(s))
-
+    
     configs = {}
-
+    
     for opt in options:
         configs[opt[0]] = opt[1]
-
+    
     return configs
 
 
-def write_config(filename, section, option, value):
+def write_config(section, option, value):
     '''
-
+    
     This function will write the configuration to the file.
-
+    
     '''
-
+    
+    filename = HOME + CONFIG_FILE
+    
     config = ConfigParser()
     config.read(filename)
-
+    
     if not config.has_section(section):
         config.add_section(section)
-
+    
     with open(filename, "w") as file:
         config.set(section, option, value)
         config.write(file)
-
+    
     return section, option, value
-
-
+    
+    
 def show_config():
     '''
-
+    
     This function will display the configs located in the
     configuration file.
-
+    
     '''
-
-    configs = load_config_file(HOME + CONFIG_FILE)
+    
+    configs = load_config_file()
     print ""
-
+    
     for key, value in configs.items():
         if key == 'password':
             msg = key.capitalize() + ":\t" + colorize(str(value), CYAN)
@@ -102,5 +104,5 @@ def show_config():
             print msg
         else:
             print key.capitalize() + ":\t" + colorize(str(value), CYAN)
-
+    
     print ""
--- uptime/prompt.py
+++ uptime/prompt.py
@@ -2,6 +2,8 @@
 
 
 from uptime.__init__ import __website_name__ as WEBSITE_NAME
+from uptime.__init__ import __config_file__ as CONFIG_FILE
+from uptime.__init__ import __home_folder__ as HOME
 from uptime.validators import *
 from uptime.colours import *
 from uptime.config import *
@@ -21,90 +23,93 @@ Welcome to the uptime client configurator.
 
 We inite you to visite {website} to register an account.
 If you already have an account please enter the following
-information.
+information.    
 
 '''
 
 
 def header():
     '''
-
+    
     This function display the header message.
-
+    
     '''
-
+        
     print infos(INTRO.format(website=WEBSITE_NAME))
 
 
 def prompt_password():
     '''
-
+    
     This function will display the prompt to collect the password of
     the end-user. Then it will validate the password.
-
+    
     '''
-
+    
     password = None
-
+    
     while not password:
         user_input = getpass(prompt="Password: ")
-
+        
         if validate_password(user_input):
             password = user_input
         else:
             message = "Please enter a valid password."
             print errors(message)
-
+        
     return password
-
+    
 
 def prompt_username():
     '''
-
+    
     This function will display the prompt to collect the username of
     the end-user. Then it will validate the username.
-
+    
     '''
     username = None
-
+    
     while not username:
         user_input = raw_input("Username: ")
-
+        
         if validate_email(user_input):
             username = user_input
         else:
             message = "Please enter a valid username (email address)."
             print errors(message)
-
+        
     return username
-
+    
 
 DEFAULT_FOOTER = '''
 The configurations have been written to the file:
 
     {filename}
-
+    
 \033[35mThank you for using: {website}!\033[0m
 '''
-
-
-def footer(filename):
+    
+    
+def footer():
     '''
-
+    
     This function display the footer message.
-
+    
     '''
-
-    print DEFAULT_FOOTER.format(website=WEBSITE_NAME, filename=filename)
-
-
+    
+    filename = HOME + CONFIG_FILE
+        
+    print DEFAULT_FOOTER.format(website=WEBSITE_NAME, 
+            filename=filename)
+            
+    
 def prompt_logic():
     '''
-
+    
     This function print the prompt in order.
-
+    
     '''
-
+    
     try:
         header()
         # request the username and password
@@ -116,11 +121,11 @@ def prompt_logic():
         # try to write the config to the file
         filename = check_config_file()
         # try to write to the configuration
-        write_config(filename, 'account', 'username', username)
-        write_config(filename, 'account', 'password', password)
+        write_config('account', 'username', username)
+        write_config('account', 'password', password)
         # Display where it wrote the file
-        footer(filename)
-
+        footer()
+        
     except KeyboardInterrupt:
         username = None
         password = None
