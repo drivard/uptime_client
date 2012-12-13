ce6d58a7802c299d5bd1690996ce333613ee0310
Dominick Rivard <dominick.rivard@gmail.com>
2012-12-13 00:01:46 -0500

    Dev:: unittest suite, bare with me this is my first attemp at writing tests. I used coverage.py to report on the test I am writing. So far I am covering a 100% of the files I am testing, but it doesn't report on the rest of the code present in this little app.

---
+++ uptime/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+'''
+
+Python Unit tests.
+
+'''
+
+from uptime.test.test_validators import test_validators
+from uptime.test.test_colours import test_colours
+import unittest
+import random
+
+class test_random_number(unittest.TestCase):
+    
+    def setUp(self):
+        # This test is a random test and it does not cover any code of 
+        # the application, but it makes sure the unittest module works.
+        self.seq = range(10)
+        
+    def test_shuffle(self):
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))      
+    
+        
+if __name__ == '__main__':
+    print "Unittest::functionality"
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_random_number)
+    # run the unit tests
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    print '\n'
+    
+    print "Unittest::validators.py"
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_validators)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    print '\n'
+    
+    print "Unittest::colours.py"
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_colours)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    print '\n'
